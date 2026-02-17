f98b987d2e574f04517912106e9e5b353c3025af
Coder X Unknown <coderxunknown0@gmail.com>
2026-02-17 07:23:59 +0530

    Refactor WebView initialization in MainActivity

--- app/src/main/java/com/dark/client/MainActivity.kt
+++ app/src/main/java/com/dark/client/MainActivity.kt
@@ -2,27 +2,23 @@ package com.dark.client
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var webView: WebView
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        webView = WebView(this)
+        val webView = WebView(this)
         setContentView(webView)
 
-        val settings: WebSettings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.userAgentString =
-            "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Mobile Safari/537.36"
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
 
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://discord.com/app")
