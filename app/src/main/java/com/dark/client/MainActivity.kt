db21dccd15a05462738e222156903d785ee3044d
Coder X Unknown <coderxunknown0@gmail.com>
2026-02-17 07:36:09 +0530

    Refactor WebView settings and enable mixed content
    Refactor WebView settings for better configuration and added support for mixed content on Lollipop and above.

--- app/src/main/java/com/dark/client/MainActivity.kt
+++ app/src/main/java/com/dark/client/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dark.client
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -15,12 +18,22 @@ class MainActivity : AppCompatActivity() {
         val webView = WebView(this)
         setContentView(webView)
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.useWideViewPort = true
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.databaseEnabled = true
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.allowFileAccess = true
+        settings.mediaPlaybackRequiresUserGesture = false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
 
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
         webView.loadUrl("https://discord.com/app")
     }
 }
